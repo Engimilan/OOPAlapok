e8bbdecf85caccaf6a0189fc03079c50ed358063
engi <engim@kkszki.hu>
2025-10-01 12:28:32 +0200

    Opp

--- OOAlapok/Program.cs
+++ OOAlapok/Program.cs
@@ -8,12 +8,13 @@ namespace OOAlapok
 {
     public class Szemely
     {
-        private string _nev;
-        private int _kor;
+        protected string _nev;
+        protected int _kor;
 
-        public Szemely(string nev) 
+        public Szemely(string nev, int kor) 
         {
             _nev = nev;
+            _kor = kor;
           
         }
         public string Nev
@@ -39,17 +40,103 @@ namespace OOAlapok
             return $"A tanulo neve {_nev} életkora {_kor}";
         }
     }
+        
+    class bankszamla
+    {
+        private int _egyenleg;
 
+        public int Egyenleg
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    _egyenleg = value;
+                }
+                else
+                {
+                    Console.WriteLine("Nem lehet 0-nal kisebb");
+                }
+            }
+        }
+        public void Betesz()
+        {
+
+        }
+
+        public void Kivesz()
+        {
+
+        }
+    }
+
+    class Hallgato : Szemely
+    {
+        private string _neptunkod;
+
+        public string Neptunkod
+        {
+            get { return _neptunkod; }
+            set 
+            { 
+                if(value.Length<=6)
+                    _neptunkod = value; 
+            }
+        }
+        public Hallgato(string nev, int kor) : base(nev, kor) 
+        {
+            
+        }
+    }
+
+    class Dolgozo : Szemely
+    {
+        private int _ber;
+
+        public Dolgozo(string nev, int kor, int ber) : base(nev, kor)
+        {
+            _ber = ber;
+        }
+        public override string ToString()
+        {
+            return $"{Nev} {Kor} {_ber}";
+        }
+    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo1 = new Szemely("Kiss Ilona");
-            tanulo1.Kor = 34;
+            Szemely tanulo1 = new Szemely("Kiss Ilona", 34);
 
             Console.WriteLine(tanulo1);
             //Console.WriteLine(tanulo1.Nev+tanulo1.Kor);
+
+            List<Hallgato> hallgatoLista = new List<Hallgato>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write($"Kérem {i+1} nevet: ");
+                string nev= Console.ReadLine();
+
+                Console.Write($"Kérem {i + 1} tanulo korát: ");
+                int kor = int.Parse(Console.ReadLine());
+                Hallgato tanulo = new Hallgato(nev, kor);
+
+                Console.Write($"Kérem a {i+1}. tanulo neptunkódját: ");
+                string neptunkod = Console.ReadLine();
+                tanulo.Neptunkod = neptunkod;
+
+                hallgatoLista.Add( tanulo );
+            }
+
+            foreach (var item in hallgatoLista)
+            {
+                Console.WriteLine(item.Nev);
+            }
+
+            Dolgozo dolgozo1 = new Dolgozo("Lapos Elemér", 43, 7500);
+            Console.WriteLine(dolgozo1);
         }
     }
 }
